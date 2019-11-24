57b9995fd80ab2c605e66c0ba10d588f9347bed1
phonguet <40883370+phonguet@users.noreply.github.com>
2019-11-24 22:58:03 +0700

    Update endcode.py

--- bai4/endcode.py
+++ bai4/endcode.py
@@ -4,7 +4,7 @@ import imageio
 from scipy.fftpack import fft2
 from scipy.fftpack import ifft2
 
-im = plt.imread('D:\Kỳ 1 năm 4\Truyền thông đa phương tiện\BTLT-nhom6-master\Bai4\endcode_test.png')
+im = plt.imread('D:\Kỳ 1 năm 4\Truyền thông đa phương tiện\BTLT-Nhom6\Bai4\endcode_test.png')
 im_size = im.shape[0] + im.shape[1] + im.shape[2]
 fft_im = fft2(im)
 imageio.imwrite('real.tif', fft_im.real)
