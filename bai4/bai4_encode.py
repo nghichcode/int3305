83e2f5e1fbe1a91e9d894f81cc56abd65a4d497d
nghichcode <nghichcode@gmail.com>
2019-11-24 22:23:51 +0700

    NextStage=1.8

--- bai4/bai4_encode.py
+++ bai4/bai4_encode.py
@@ -6,11 +6,11 @@ im_inp = imread('image_inp.png')
 aresh = (im_inp.shape[0]*im_inp.shape[1])
 scale = 1.0/aresh
 
+ffreq = lambda dt : rfft(rfft(dt, axis=0), axis=1)
 im_dr = ffreq(im_inp)
 im_freq = (im_dr*scale+1.0)/2.0
 imsave('./image_out_freq.png', im_freq)
 
-# ffreq = lambda dt : rfft(rfft(dt, axis=0), axis=1)
 #
 # from scipy.fftpack import fft2, ifft2
 # im_gray1d = np.average(im_inp[..., :3], axis=2)
