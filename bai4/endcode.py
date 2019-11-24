67176bcdda8132b544df70de7aa33ad748d5a618
phonguet <40883370+phonguet@users.noreply.github.com>
2019-11-24 23:01:30 +0700

    Update endcode.py

--- bai4/endcode.py
+++ bai4/endcode.py
@@ -7,5 +7,6 @@ from scipy.fftpack import ifft2
 im = plt.imread('D:\Kỳ 1 năm 4\Truyền thông đa phương tiện\BTLT-Nhom6\Bai4\endcode_test.png')
 im_size = im.shape[0] + im.shape[1] + im.shape[2]
 fft_im = fft2(im)
+# Đã thử lưu file ảnh là .png nhưng ảnh ra không như mong muốn
 imageio.imwrite('real.tif', fft_im.real)
 imageio.imwrite('imagined.tif', fft_im.imag)
